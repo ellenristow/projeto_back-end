cf7d4cd6b11e68d1ee3710abc93892f3e7d56fee
Ellen Ristow <ellen.ristow@gmail.com>
2024-10-07 20:08:25 +0100

    create files in views directory. create api file in controller. create base.php file in models

---
+++ models/base.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Base 
+{
+    public $db;
+
+    public function __construct() {
+
+        $this->db = new PDO(
+            "mysql:host=" . ENV["DB_HOST"] . ";dbname=" . ENV["DB_NAME"] . ";charset=utf8",
+            ENV["DB_USER"],
+            ENV["DB_PASSWORD"],
+            [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_STRINGIFY_FETCHES => false
+            ]
+        );
+    }
+
+    public function isValidKey($api_key){
+
+        $query = $this->db->prepare("
+            SELECT 
+                user_id
+            FROM 
+                users
+            WHERE api_key = ?
+        ");
+
+        $query->execute([$api_key]);
+
+        $user = $query->fetch();
+
+        return !empty($user);
+    }
+}
