c7e4cec49c0e3ae78947cb4c1848b3a1693992a3
Ellen Ristow <ellen.ristow@gmail.com>
2024-10-12 21:52:54 +0100

    modify controller/home

--- controllers/home.php
+++ controllers/home.php
@@ -6,6 +6,11 @@ require("models/likes.php");
 $model = new Recipes();
 $recipes = $model->get();
 
-
+$likesModel = new Likes();
+foreach($recipes as $key => &$recipe) {
+    $likes = $likesModel->getLikesByRecipe($recipe["recipe_id"]);
+      var_dump($likes);  
+    $recipe["like_count"] = $likes["like_count"];
+}
 
 require("views/home.php");
