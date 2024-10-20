65b1ba26b84e75c451cfbce35e61c3539cc5a046
Ellen Ristow <ellen.ristow@gmail.com>
2024-10-20 10:24:53 +0100

    change head

--- views/login.php
+++ views/login.php
@@ -1,5 +1,10 @@
-<?php include "views/templates/head.php"; ?>
-
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marmita</title>
+</head>
 <body>
     <h1><a href="<?php ROOT ?>/">Bem Vindo Marmiteiro!</a></h1>
     <main>
